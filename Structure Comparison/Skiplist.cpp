c2469487c44376642d6d3a3dd77ae14467f7518c
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-03-23 14:29:30 -0400

    Work on skiplist

---
+++ Structure Comparison/Skiplist.cpp
@@ -0,0 +1,61 @@
+#include "Skiplist.h"
+#include <stdlib.h>
+
+Skiplist::Skiplist()
+{
+	head = new node;
+	tail = new node;
+	strcpy(head->word, node::NEG_INF);
+	strcpy(tail->word, node::POS_INF);
+
+	head->right = tail;
+	tail->left = head;
+
+	height = 1;
+	numItems = 0;
+}
+
+Skiplist::~Skiplist()
+{
+
+}
+
+Skiplist::node* Skiplist::find(const char word[50])
+{
+	node* p = head;
+	while (true)
+	{
+		while (strcmp(p->right->word, node::POS_INF) != 0 && strcmp(p->right->word, word) <= 0)
+			p = p->right;
+		if (p->down == nullptr) return p;
+		p = p->down;
+	}
+}
+
+void Skiplist::insert(const char word[50])
+{
+	node* p = find(word);
+	if (strcmp(p->word, word) == 0)
+	{
+		p->count++;
+	}
+	else
+	{
+		node* newNode = new node;
+		// defaults
+		strcpy(newNode->word, word);
+		p->right->left = newNode;
+		p->right = newNode;
+
+		int currentHeight = 1;
+
+		while (rand() & 1) // seed this somehow eventually
+		{
+			currentHeight++;
+			if (currentHeight > height)
+			{
+				
+			}
+		}
+	}
+}
--- Structure Comparison/Skiplist.h
+++ Structure Comparison/Skiplist.h
@@ -1,1 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
 #pragma once
+#include <string.h>
+using namespace std;
+class Skiplist
+{
+public:
+	Skiplist();
+	~Skiplist();
+	void insert(const char word[50]);
+	void traverse();
+private:
+	struct node
+	{
+		char word[50];
+		int count = 1;
+		node* left = nullptr;
+		node* right = nullptr;
+		node* up = nullptr;
+		node* down = nullptr;
+
+		const static char NEG_INF[50];
+		const static char POS_INF[50];
+	};
+
+	node* head = nullptr;
+	node* tail = nullptr;
+	int height;
+	int numItems;
+	void list(int& index, node* n);
+	node* find(const char word[50]);
+
+	void deleteNode(node* n);
+};
