a814c48d7ce44619a47df9f5e26d69386488263b
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-03-24 12:23:52 -0400

    Skiplist insert I think

--- Structure Comparison/Skiplist.cpp
+++ Structure Comparison/Skiplist.cpp
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+const char Skiplist::node::NEG_INF[50] = { '.' };
+const char Skiplist::node::POS_INF[50] = { ',' };
+
 Skiplist::Skiplist()
 {
-	head = new node;
-	tail = new node;
-	strcpy(head->word, node::NEG_INF);
-	strcpy(tail->word, node::POS_INF);
+	head = createNegInfNode();
+	tail = createPosInfNode();
 
 	head->right = tail;
 	tail->left = head;
@@ -23,14 +24,32 @@ Skiplist::~Skiplist()
 
 }
 
+Skiplist::node* Skiplist::createNegInfNode()
+{
+	node* newNode = new node;
+	strcpy(newNode->word, node::NEG_INF);
+	return newNode;
+}
+
+Skiplist::node* Skiplist::createPosInfNode()
+{
+	node* newNode = new node;
+	strcpy(newNode->word, node::POS_INF);
+	return newNode;
+}
+
 Skiplist::node* Skiplist::find(const char word[50])
 {
 	node* p = head;
 	while (true)
 	{
 		while (strcmp(p->right->word, node::POS_INF) != 0 && strcmp(p->right->word, word) <= 0)
+		{
 			p = p->right;
+		}
+
 		if (p->down == nullptr) return p;
+
 		p = p->down;
 	}
 }
@@ -38,27 +57,66 @@ Skiplist::node* Skiplist::find(const char word[50])
 void Skiplist::insert(const char word[50])
 {
 	node* p = find(word);
+
 	if (strcmp(p->word, word) == 0)
 	{
 		p->count++;
+		return;
 	}
-	else
+	node* newNode = new node;
+	// defaults
+	strcpy(newNode->word, word);
+
+	newNode->left = p;
+	newNode->right = p->right;
+
+	p->right->left = newNode;
+	p->right = newNode;
+
+	numItems++;
+
+	int currentHeight = 1;
+
+	while (rand() & 1) // seed this somehow eventually
 	{
-		node* newNode = new node;
-		// defaults
-		strcpy(newNode->word, word);
-		p->right->left = newNode;
-		p->right = newNode;
+		currentHeight++;
 
-		int currentHeight = 1;
-
-		while (rand() & 1) // seed this somehow eventually
+		if (currentHeight > height)
 		{
-			currentHeight++;
-			if (currentHeight > height)
-			{
-				
-			}
+			node* negInf = createNegInfNode();
+			node* posInf = createPosInfNode();
+
+			negInf->down = head;
+			head->up = negInf;
+
+			posInf->down = tail;
+			tail->up = posInf;
+
+			negInf->right = posInf;
+			posInf->left = negInf;
+
+			head = negInf;
+			tail = posInf;
+
+			height++;
 		}
+
+		node* stackNode = new node; // Node that gets stacked on top of the current node
+		strcpy(stackNode->word, word);
+
+		node* leftNode = p; 
+		while (leftNode->up == nullptr)
+		{
+			leftNode = leftNode->left;
+		}
+
+		node* rightNode = p;
+		while (rightNode->up == nullptr)
+		{
+			rightNode = rightNode->right;
+		}
+
+		stackNode->left = leftNode->up;
+		stackNode->right = rightNode->up;
 	}
 }
